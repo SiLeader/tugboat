8aeee6b79093f2390af4248dee1941033607f67e
SiLeader <sileader.dev@gmail.com>
2026-03-17 12:29:25 +0000

    add controller manager

---
+++ tugboat-controller-manager/src/manager.rs
@@ -0,0 +1,34 @@
+use crate::base::TugboatController;
+use tracing::info;
+
+pub(crate) struct TugboatControllerManager {
+    controllers: Vec<Box<dyn TugboatController>>,
+}
+
+impl TugboatControllerManager {
+    pub(crate) fn new() -> Self {
+        Self {
+            controllers: vec![],
+        }
+    }
+
+    pub(crate) async fn setup(&mut self) {
+        info!("Setting up controllers");
+        for controller in self.controllers.iter_mut() {
+            info!("Setting up '{}' controller", controller.name());
+            controller.setup().await;
+        }
+    }
+
+    pub(crate) async fn run(self) {
+        info!("Running controllers");
+        let mut handle = Vec::with_capacity(self.controllers.len());
+        for controller in self.controllers {
+            info!("Spawning '{}' controller", controller.name());
+            handle.push(tokio::spawn(async move { controller.run().await }));
+        }
+        for handle in handle {
+            handle.await.unwrap();
+        }
+    }
+}
